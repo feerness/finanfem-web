17ecee0667642f3faa00a98820c6b63dbf2a342b
ConiNejo <conimalebran@gmail.com>
2024-08-25 17:42:57 -0400

    mis modificaciones

--- frontend/src/components/ui/Button.jsx
+++ frontend/src/components/ui/Button.jsx
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 export function Button({ onClick, children }) {
     return (
       <button
-        className="bg-pink-500 px-4 py-1 rounded-md my-2 disabled:bg-pink-300"
+        className=""
         onClick={onClick}
       >
         {children}
--- frontend/src/components/ui/ButtonLink.jsx
+++ frontend/src/components/ui/ButtonLink.jsx
@@ -2,7 +2,7 @@ import PropTypes from 'prop-types';
 import { Link } from "react-router-dom";
 
 export const ButtonLink = ({ to, children }) => (
-  <Link to={to} className="bg-purple-950 px-4 py-1 rounded-md">
+  <Link to={to} className="">
     {children}
   </Link>
 );
