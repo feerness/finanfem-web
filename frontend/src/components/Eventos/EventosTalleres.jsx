071802d991decf28eb695ae27b653cd212992b9f
pauletteramirez96@gmail.com <pauletteramirez96@gmail.com>
2024-08-28 19:06:53 -0400

    arreglos eventos y talleres

--- frontend/src/components/Eventos/EventosTalleres.jsx
+++ frontend/src/components/Eventos/EventosTalleres.jsx
@@ -38,7 +38,7 @@ const EventosTalleres = () => {
   const events = [
     {
       title: "Programa Mujeres Jefas de Hogar",
-      image: "frontend/public/images/card-eventos-1.jpg",
+      image: "/images/card-eventos-1.jpg",
       description:
         "El programa busca promover la autonomía económica de las mujeres jefas de hogar, a través de un conjunto de herramientas que les permitan generar y gestionar ingresos y recursos propios a partir del trabajo remunerado, el acceso a la oferta pública y a oportunidades de conciliación trabajo remunerado, doméstico y de cuidados.",
       link: "https://www.chileatiende.gob.cl/fichas/12885-programa-mujeres-jefas-de-hogar",
@@ -46,28 +46,28 @@ const EventosTalleres = () => {
     {
       title:
         "Talleres de Formación y Capacitación en oficios",
-      image: "frontend/public/images/card-eventos-2.jpg",
+      image: "/images/card-eventos-2.jpg",
       description:
         "Durante todos estos años de existencia de la Casa de la Mujer de Huamachuco, han sido miles de mujeres quienes se han capacitado e ingresado al mundo laboral, mejorando su calidad de vida y la de sus familias..",
       link: "https://www.casadelamujer.cl/servicios/talleres-de-formacion-y-capacitacion-en-oficios/",
     },
     {
       title: "Prodemu lanzó talleres gratuitos para 45 mil mujeres del país este 2024",
-      image: "frontend/public/images/card-eventos-3.jpg",
+      image: "/images/card-eventos-3.jpg",
       description:
         "Se trata de cursos que abordan distintas áreas, tales como el empoderamiento, la autonomía económica y el ejercicio de derechos, en 56 provincias y 16 regiones a lo largo de todo el territorio nacional.",
       link: "https://minmujeryeg.gob.cl/?p=53173",
     },
     {
       title: "Talleres Abiertos para el Crecimiento Personal",
-      image: "frontend/public/images/card-eventos-4.jpg",
+      image: "/images/card-eventos-4.jpg",
       description:
         "Si tienes interés por desarrollar el conocimento sobre tí misma, saber cómo nos afecta la educación de género y participar junto con otras mujeres en desmontar todas las ideas que nos mantienen atadas a la dependencia, la inseguridad, la sobrecarga, la falta de asertidad, etc… Los Talleres Abiertos para el Crecimento Personal de Mujeres para la Salud te proponen esta sugerente posibilidad.",
       link: "https://www.mujeresparalasalud.org/category/servicios-de-mujeres-para-la-salud/talleres-abiertos-para-el-crecimiento-personal/",
     },
     {
       title: "Calendario de eventos económicos",
-      image: "frontend/public/images/card-eventos-5.jpg",
+      image: "/images/card-eventos-5.jpg",
       description:
         "Calendario oficial de Banco Central de Chile, con las fechas de todos los eventos económicos.",
       link: "https://www.bcentral.cl/es/web/banco-central/noticias-y-publicaciones/prensa/calendario-eventos-economicos",
@@ -75,7 +75,7 @@ const EventosTalleres = () => {
     {
       title:
         "Chile Fintech Forum 2024",
-      image: "frontend/public/images/card-eventos-6.jpg",
+      image: "/images/card-eventos-6.jpg",
       description:
         "Chile Fintech Forum 2024, es una plataforma que genera y promueve el intercambio de conocimientos, experiencias y tecnologías que permiten al sector público y privado alcanzar nuevos niveles de innovación, competencia e inclusión financiera en el país, posicionando a su vez a Chile como referente en estos temas.",
       link: "https://www.chilefintechforum.com",
